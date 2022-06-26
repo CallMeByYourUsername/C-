af814d42374d525fb64d51ac0266e04d6282a709
Viktoria <victrofimowa303@gmail.com>
2022-06-26 19:07:42 +0300

    Homework 2 Task 10

---
+++ Homework2.10/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+Console.WriteLine("Insert 3-digits number: ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 99 && number < 1000)
+{
+int secondDigit = number / 10 % 10;
+
+Console.WriteLine(secondDigit);
+}
+else 
+Console.WriteLine("Error");
