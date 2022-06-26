fa9d28849863533bc61d0924885da74be6fae7c9
Viktoria <victrofimowa303@gmail.com>
2022-06-26 19:20:07 +0300

    Homework 2 Task 13

---
+++ Homework2.13/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Insert a number: ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 99 )
+{
+   string str = number.ToString();
+   Console.WriteLine(str[2]);
+}
+else 
+Console.WriteLine("You don't have the third number");
