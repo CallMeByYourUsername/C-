403c1e66346984c32e6fd06c6a91affcc807c7d6
Viktoria <victrofimowa303@gmail.com>
2022-06-26 11:09:04 +0300

    Homework 1, task 2

---
+++ Homework1.2/Program.cs
@@ -0,0 +1,17 @@
+﻿int numberA = 10;
+int numberB = 3;
+ 
+if (numberA > numberB) 
+{
+Console.Write("Max number is: ");    
+Console.WriteLine(numberA);
+Console.Write("Min number is: ");
+Console.WriteLine(numberB);
+}
+else 
+{
+    Console.Write("Max number is: ");
+    Console.WriteLine(numberB);
+    Console.Write("Min number is: ");
+    Console.WriteLine(numberA);
+}
