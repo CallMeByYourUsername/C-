6ca43fbdc4ccf204ec44a7a9243c2ad888eaea96
Viktoria <victrofimowa303@gmail.com>
2022-06-26 19:34:53 +0300

    Homework 2 Task 15

---
+++ Homework2.15/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Insert a number: ");
+
+int day = Convert.ToInt32(Console.ReadLine());
+
+switch(day)
+{
+case 1: Console.WriteLine("no");
+break;
+case 2: Console.WriteLine("no");
+break;
+case 3: Console.WriteLine("no");
+break;
+case 4: Console.WriteLine("no");
+break;
+case 5: Console.WriteLine("no");
+break;
+case 6: Console.WriteLine("yes");
+break;
+case 7: Console.WriteLine("yes");
+break;
+default: Console.WriteLine("Error");
+break;    
+}
+
+
+
+
