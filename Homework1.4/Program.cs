c86c022145ef6108396e85cd8262d1d078e3a31b
Viktoria <victrofimowa303@gmail.com>
2022-06-26 11:22:04 +0300

    Homework 1 task 4

---
+++ Homework1.4/Program.cs
@@ -0,0 +1,16 @@
+﻿int numberA = 222;
+int numberB = 40;
+int numberC = 33;
+
+int max = numberA;
+
+if (numberB > max)
+ {
+    max = numberB;
+ }
+if (numberC > max)
+ {
+    max = numberC;
+ }
+else 
+Console.WriteLine(max);
