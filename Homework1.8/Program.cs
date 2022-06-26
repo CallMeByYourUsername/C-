09577aa6791e69bfea8f6ad92b7fc3e26f9f46e2
Viktoria <victrofimowa303@gmail.com>
2022-06-26 13:22:59 +0300

    Homework 1 Task 4

---
+++ Homework1.8/Program.cs
@@ -0,0 +1,31 @@
+﻿ Console.WriteLine("Write a number: ");
+ int n = int.Parse(Console.ReadLine()!);
+ int x = 2; 
+
+ while (x < n) 
+ {
+    Console.WriteLine(x);
+    x+=2;
+ }
+  Console.WriteLine(x);
+
+
+// Я бы сделала так, но нельзя сделать массив со значением index:
+/*int[] arr = new int[index];
+int n = 8;
+int index = 0;
+int evenNumbers = 2;
+
+if (evenNumbers < n) 
+{
+ arr[index] = evenNumbers;
+ index ++;
+ evenNumbers+=2;
+}
+else 
+{
+    Console.WriteLine(arr[index]);
+} */ 
+
+
+
