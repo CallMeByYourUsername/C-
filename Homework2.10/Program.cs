ab78564191e4c82e6df034dbe909629985e06805
Viktoria <victrofimowa303@gmail.com>
2022-06-29 14:24:37 +0300

    Homework 2 Task 10 Changed

--- Homework2.10/Program.cs
+++ Homework2.10/Program.cs
@@ -6,9 +6,9 @@ int number = Convert.ToInt32(Console.ReadLine());
 
 if (number > 99 && number < 1000)
 {
-int secondDigit = number / 10 % 10;
-
-Console.WriteLine(secondDigit);
+Console.WriteLine(number / 10 % 10);
 }
 else 
+{
 Console.WriteLine("Error");
+}
