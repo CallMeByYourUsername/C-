a4543007bd3716538629ac513c4d9b91bae107e5
Viktoria <victrofimowa303@gmail.com>
2022-07-03 15:42:23 +0300

    Homework 3 Task 23

---
+++ Homework3.23/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Write a number");
+int number = Convert.ToInt32(Console.ReadLine());
+int a = 1;
+while (a <= number)
+{
+    Console.Write($"{a*a*a};");
+    a++;
+}
+Console.Write("\b \b");
+
