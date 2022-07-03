9cb9843807670efc79aa37a43d02bfbeeea0120e
Viktoria <victrofimowa303@gmail.com>
2022-07-03 15:20:09 +0300

    Homework 3 Task 21

---
+++ Homework3.21/Program.cs
@@ -0,0 +1,10 @@
+﻿void getCoordinates (double xa, double ya, double za, double xb, double yb, double zb)
+{
+    double x = xa - xb;
+    double y = ya - yb;
+    double z = za - zb;
+    double c = Math.Sqrt(x*x + y*y + z*z);
+    Console.WriteLine(Math.Round(c,2));
+}
+getCoordinates(3, 6, 8, 2, 1, -7);
+
