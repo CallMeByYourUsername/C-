a972a3faf1b1d3d93bac5a9ac3ebbf238751a293
Viktoria <victrofimowa303@gmail.com>
2022-07-03 14:59:23 +0300

    Homework 3 Task 19

---
+++ Homework3.19/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Write a 5-digit number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num > 9999 && num < 100000)
+{
+    string str = num.ToString();
+    if (str [0] == str [4] && str[1] == str[3])
+    {
+        Console.WriteLine("it is a palindrome");
+    }
+    else 
+    {
+    Console.WriteLine("It is not a palindrome");
+    }
+}
+else 
+{
+    Console.WriteLine("Error, use only 5 digits");
+}
+   
+  
