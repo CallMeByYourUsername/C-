d1679b725faef84138feed641f8e85769c379885
Viktoria <victrofimowa303@gmail.com>
2022-07-04 19:36:05 +0300

    Homework 4 Task 25

---
+++ Homework4.25/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Write the first number: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Write the second number: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+int GetMult(int numberA, int numberB)
+{
+int result = 1;
+int i = 0;
+for (i = 1; i <= numberB; i++)
+{
+      result = result * numberA;
+}
+return result;
+}
+int result = GetMult(numberA, numberB);
+Console.WriteLine(result);
