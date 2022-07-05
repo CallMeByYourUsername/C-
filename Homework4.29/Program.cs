7d670d140511f4e1713286b75821c1c931eadf13
Viktoria <victrofimowa303@gmail.com>
2022-07-05 14:00:51 +0300

    Homework 4 Task 29

---
+++ Homework4.29/Program.cs
@@ -0,0 +1,6 @@
+﻿// Я не уверена что по условию задачи требовалось именно это, но вот что получилось 
+ int[] array = new int[8];
+Random rand = new Random();
+for (int i = 0; i < array.Length; i++)
+   array[i] = rand.Next(1, 100); 
+ Console.WriteLine("[{0}]", string.Join(", ", array));
