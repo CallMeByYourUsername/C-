cb5ba62946ced31cef192e3e48447e53e0cc1930
Viktoria <victrofimowa303@gmail.com>
2022-07-05 13:44:35 +0300

    Homework 4 Task 27

---
+++ Homework4.27/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Write a number: ");
+ int a = Convert.ToInt32(Console.ReadLine());
+            int s = 0;
+            while (a > 0)
+            {
+ 
+                s = s + a % 10;
+                a = a /10 ;
+ 
+            }
+ Console.WriteLine(s);
