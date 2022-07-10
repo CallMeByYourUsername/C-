d5eb2378d71bfda472078f420c21d03775b9b88a
Viktoria <victrofimowa303@gmail.com>
2022-07-10 17:53:33 +0300

    Classwork 9.07

---
+++ Classwork1/Program.cs
@@ -0,0 +1,14 @@
+﻿ int[] array = new int[123];
+Random rand = new Random();
+for (int i = 0; i < array.Length; i++)
+   array[i] = rand.Next(200); 
+ Console.WriteLine("[{0}]", string.Join(", ", array));
+
+ int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+     if (array[i] >= 10 && array[i] <= 99)
+         count ++;
+    }
+
+    Console.WriteLine(count);
