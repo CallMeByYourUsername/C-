a35a4b85e7224c5d72c4b86d62e2eaa1a19b2cd0
Viktoria <victrofimowa303@gmail.com>
2022-07-10 20:26:41 +0300

    Homework 5 Task 38

---
+++ Homework5.38/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задайте массив вещественных чисел. 
+//Найдите разницу между максимальным и минимальным элементов массива.
+
+float [] array = new float [8];
+Random rand = new Random();
+for (int i = 0; i < array.Length; i ++)
+array[i] = rand.Next(200);
+Console.WriteLine(array);
+Console.WriteLine("[{0}]", string.Join(", " , array));
+
+float min = array[0]; 
+float max = array[0]; 
+for ( int i = 0; i < array.Length; i ++)
+{ 
+   if (array[i] < min)
+   {
+    min = array[i];
+   }
+   else if (array[i] > max) 
+   {
+    max = array [i];
+   }
+}
+Console.WriteLine(max-min);
