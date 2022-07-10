62d3202e351b32921e2b6ecef0b799c284d2565e
Viktoria <victrofimowa303@gmail.com>
2022-07-10 19:17:34 +0300

    Homework 5 Task 36

---
+++ Homework5.36/Program.cs
@@ -0,0 +1,15 @@
+﻿//Задайте одномерный массив, заполненный случайными числами. 
+//Найдите сумму элементов, стоящих на нечётных позициях.
+
+int [] array = new int [3];
+Random rand = new Random();
+for (int i =0; i < array.Length; i++)
+array [i] = rand.Next(200);
+Console.WriteLine("[{0}]", string.Join(", " , array));
+
+int sum = 0;
+for (int i = 1; i < array.Length; i +=2)
+{
+    sum += array[i];
+}
+Console.WriteLine(sum);
