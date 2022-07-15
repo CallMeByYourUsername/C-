67b48e253e811bea28a1e34d9760a9037340b0f2
Viktoria <victrofimowa303@gmail.com>
2022-07-15 12:17:17 +0300

    Homework 6  Task 43 Changed

--- Homework6.43/Program.cs
+++ Homework6.43/Program.cs
@@ -2,11 +2,21 @@
 // y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
 
 Console.WriteLine("Write four coordinates in the following order(k1, k2, b1, b2): ");
-float k1 = Convert.ToInt32(Console.ReadLine());
-float k2 = Convert.ToInt32(Console.ReadLine());
-float b1 = Convert.ToInt32(Console.ReadLine());
-float b2 = Convert.ToInt32(Console.ReadLine());
-float x = (b2 - b1) / (k1 - k2);
-float y = k1 * x + b1;
-float y1 = k2 * x + b2;
-Console.Write($"{y}, {y1}");
+double k1 = Convert.ToInt32(Console.ReadLine());
+double k2 = Convert.ToInt32(Console.ReadLine());
+double b1 = Convert.ToInt32(Console.ReadLine());
+double b2 = Convert.ToInt32(Console.ReadLine());
+
+if (k1 == k2)
+{
+    if (k1 == k2 && b1 == b2) Console.WriteLine($"Lines match");
+    else Console.WriteLine($"Lines are parallel");
+}
+else
+{
+    double x = (-b2 + b1) / (-k1 + k2);
+    double y = k2 * x + b2;
+    Console.WriteLine($"intersection points: [{x};{y}]"); 
+}
+
+
