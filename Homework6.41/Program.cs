7a3b11d9885f210c82a44e2abf80f6459d80fd1d
Viktoria <victrofimowa303@gmail.com>
2022-07-15 12:21:08 +0300

    Homework 6  Task 41

---
+++ Homework6.41/Program.cs
@@ -0,0 +1,11 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+Console.WriteLine("Write some numbers with commas: ");
+string[] text = Console.ReadLine()!.Split(',');
+
+int count = 0;
+for (int i = 0; i < text.Length; i++)
+{
+    int numbers = int.Parse(text[i]);
+    if (numbers > 0) count++;
+}
+System.Console.WriteLine($"{count} numbers bigger than 0");
