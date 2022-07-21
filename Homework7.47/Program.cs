6b0b14a35a4bc777eadd5ebc0d424f5c57baa75a
Viktoria <victrofimowa303@gmail.com>
2022-07-21 09:56:14 +0300

    Homework 7  Task 47

---
+++ Homework7.47/Program.cs
@@ -0,0 +1,21 @@
+﻿/*Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+m = 3, n = 4.
+
+0,5 7 -2 -0,2
+
+1 -3,3 8 -9,9
+
+8 7,8 -7,1 9*/
+
+double[,] matrix = new double[3, 4];
+var rnd = new Random();
+for (int i = 0; i < 3; i++)
+{
+    for (int j = 0; j < 4; j++)
+    {
+        matrix[i, j] = rnd.NextDouble() * 100;
+        Console.Write("{0,6:F2}", matrix[i, j]);
+    }
+    Console.WriteLine();
+}
